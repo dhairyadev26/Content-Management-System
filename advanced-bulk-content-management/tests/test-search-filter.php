fbde31a335e8acb758a85e9f7482c9b9f639531f
dhairyadev26 <dhairyad981@gmail.com>
2025-06-15 10:20:00 +0530

    Add unit tests for search and filter

---
+++ advanced-bulk-content-management/tests/test-search-filter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Unit tests for Search Filter Class
+ *
+ * @package AdvancedBulkContentManagement
+ */
+
+class Test_Search_Filter extends WP_UnitTestCase {
+    /**
+     * Test filter_posts method
+     */
+    public function test_filter_posts() {
+        // Test code will be added here
+    }
+    
+    /**
+     * Test ajax_filter_posts method
+     */
+    public function test_ajax_filter_posts() {
+        // Test code will be added here
+    }
+}
+
+
+// Updated: 2025-06-15T10:20:00 - Add unit tests for search and filter
