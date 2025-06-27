e19249a8c79693b242fdab7aabaa4bbcac74ba2c
dhairyadev26 <dhairyad981@gmail.com>
2025-06-27 10:30:00 +0530

    Create tests for mobile view CSS

---
+++ advanced-bulk-content-management/tests/test-mobile-view-css.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Unit tests for Mobile View CSS
+ *
+ * @package AdvancedBulkContentManagement
+ */
+
+class Test_Mobile_View_CSS extends WP_UnitTestCase {
+    /**
+     * Test mobile view CSS
+     */
+    public function test_mobile_view_css() {
+        // Test code will be added here
+    }
+}
+
+
+// Updated: 2025-06-27T10:30:00 - Create tests for mobile view CSS
